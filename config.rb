7ee174f0b176674f293fb351b530dca3c685b20e
Alessandro Scoccia Pappagallo <alessandro.scoccia.pappagallo@gmail.com>
2018-02-04 17:38:46 +0000

    Initial commit

---
+++ config.rb
@@ -0,0 +1,12 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+# http_images_dir = "images"
+http_generated_images_path = "../img"
+
+relative_assets = false
+line_comments = false
+output_style = :compressed
